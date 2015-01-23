5a3815dcbfcc7dde13a9e59b601635284649e579
dukesward <ychen18@slu.edu>
2015-01-23 11:32:21 -0800

    Update filters.js

--- script/lib/filter/filters.js
+++ script/lib/filter/filters.js
@@ -1,9 +1,9 @@
 define(['angular'], function(angular) {
 	return angular.module("videoApp.Filters", [])
 	.filter("extractItem", function() {
-		var temp = [];
 		//according to input from view, data is formatted;
 		return function(input) {
+			var temp = [];
 			input.items.forEach(function(item, i) {
 				temp.push(item);
 			})
